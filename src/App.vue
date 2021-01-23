776035be9781c22611f24f4fe335c0925b2b330d
Jorge Parra <jorge.luiz.alves.parra@outlook.com>
2021-01-23 13:01:16 -0300

    nav bar

--- src/App.vue
+++ src/App.vue
@@ -13,12 +13,10 @@ export default {
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<style lang="scss">
+body {
+  padding: 0;
+  margin: 0;
+  font-family: Arial;
 }
 </style>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,18 +1,35 @@
 <template>
   <el-row>
-  <el-col :span="24">
-    <div class="grid-content bg-purple-dark">
-    <h4>Sistema de cadastro de cliente</h4>
-    </div>
-  </el-col>
+    <el-col :span="24">
+      <div class="grid-content bg-purple-dark">
+        <h2>Sistema de cadastro de cliente</h2>
+      </div>
+    </el-col>
+    <el-col :span="24">
+      <el-tabs v-model="activeName" @tab-click="handleClick">
+        <el-tab-pane v-for="page in pages" :key="page.name" :label="page.name" :to="page.route" :name="page.name" />
+      </el-tabs>
+    </el-col>
 </el-row>
 </template>
 
 <script>
 export default {
   name: 'Navbar',
-  props: {
-    msg: String
+  data() {
+    return {
+      activeName: 'Clientes',
+      pages: [
+        {
+          name: 'Clientes',
+          route: '/'
+        },
+        {
+          name: 'Cadastrar cliente',
+          route: '/add-client'
+        }
+      ]
+    }
   }
 }
 </script>
@@ -22,26 +39,20 @@ export default {
     margin-bottom: 20px;
     &:last-child {
       margin-bottom: 0;
+      padding: 5px 20px;
     }
-  }
-  .el-col {
-    border-radius: 4px;
-  }
-  .bg-purple-dark {
-    background: #99a9bf;
-  }
-  .bg-purple {
-    background: #d3dce6;
-  }
-  .bg-purple-light {
-    background: #e5e9f2;
-  }
-  .grid-content {
-    border-radius: 4px;
-    min-height: 36px;
-  }
-  .row-bg {
-    padding: 10px 0;
-    background-color: #f9fafc;
+    .el-col {
+      border-radius: 4px;
+      h2 {
+        margin: auto;
+        text-align: center;
+      }
+      .bg-purple-dark {
+        background: #99a9bf;
+      }
+      .grid-content {
+        padding: 20px;
+      }
+    }
   }
 </style>
